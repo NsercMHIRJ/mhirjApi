89c4357a11a3f5afd1a6893ef33f56fc0aa72df9
Karan Gajjar <13494911+kanulp@users.noreply.github.com>
2021-07-07 01:45:24 -0400

    update cors origin

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ origins=[*]
 """
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 #####
