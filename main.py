2b13a5b503f52d1ad95f9d3c26758f4a985141c0
NavneetBhatti <84995675+NavneetBhatti@users.noreply.github.com>
2021-08-04 12:20:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -58,10 +58,10 @@ OutputTableHistory2 = pd.DataFrame()
 MDCeqns_arrayforgraphing = pd.DataFrame()
 
 # Initialiaze Database Properties
-hostname = os.environ.get('hostname', 'mhrijhumber.database.windows.net')
+hostname = os.environ.get('hostname', 'mhirjserver.database.windows.net')
 db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '8080')))
 db_name = os.environ.get('db_name', 'MHIRJ')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'mhrij')))
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'mhirj-admin')))
 db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'KaranCool123')))
 ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
 db_driver = "ODBC Driver 17 for SQL Server"
