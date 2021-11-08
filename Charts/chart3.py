df04381836d8e69d023f214e70433873bff451a4
SangeethaSridharan <sangeethasridharan93@gmail.com>
2021-11-07 21:28:16 -0500

    Changes for chart3

---
+++ Charts/chart3.py
@@ -0,0 +1,60 @@
+
+from Charts.helper import connect_database_for_chart3
+import pandas as pd
+import numpy as np
+import re
+import json
+
+def chart3Report(aircraft_no, equation_id, CurrentFlightPhaseEnabled, fromDate, toDate):
+    print(aircraft_no, equation_id, CurrentFlightPhaseEnabled, fromDate, toDate)
+    MDCdataDF = connect_database_for_chart3(aircraft_no, equation_id, CurrentFlightPhaseEnabled, fromDate, toDate)
+    MDCdataDF["DateAndTime"] = pd.to_datetime(MDCdataDF["DateAndTime"])
+
+    if CurrentFlightPhaseEnabled == 1: #Show all, current and history
+        DateNmessageDF = MDCdataDF[["DateAndTime","Equation_ID", "Aircraft"]].copy()
+        
+    elif CurrentFlightPhaseEnabled == 0: #Only show history
+        DateNmessageDF = MDCdataDF[["DateAndTime","Equation_ID", "Aircraft", "Flight_Phase"]].copy()
+        DateNmessageDF = DateNmessageDF.replace(False, np.nan).dropna(axis=0, how='any')
+        DateNmessageDF = DateNmessageDF[["DateAndTime","Equation_ID", "Aircraft"]].copy()
+        
+    pd.to_datetime(DateNmessageDF["DateAndTime"])
+    counts = pd.DataFrame(data= DateNmessageDF.groupby(['Aircraft', "Equation_ID", "DateAndTime"]).agg(len), columns= ["Counts"])
+
+    removeParanthesisRegex = r"[()]"
+    equation_ids = re.sub(removeParanthesisRegex, '', equation_id)
+    # DatesfoundinMDCdata = counts.loc[('AC'+str(aircraft_no), equation_id)].resample('D')["Counts"].sum().index
+    valuesfoundinMDCdata = ''
+    unavailableEquationIds = []
+    for equation in equation_ids.split(','):
+        equation = re.sub(r"[' ']",'',equation)
+        try:
+            valuesfoundinMDCdata += counts.loc[('AC'+str(aircraft_no), equation)].resample('D')["Counts"].sum().to_json()
+        except:
+            unavailableEquationIds.append(equation)
+
+    if len(unavailableEquationIds) != 0:
+        valuesfoundinMDCdata = json.dumps({'UnavailableEquationIds': unavailableEquationIds})
+
+    return valuesfoundinMDCdata
+
+
+
+def datefill(datesindata, valuesindata, NumDays, enddate):
+    print('came to matplot')
+    Datatoplot = pd.date_range(end= enddate, periods= NumDays, freq= "D")
+    Datatoplot = Datatoplot.to_frame(index= False, name = "Date")
+    Datatoplot["Values"] = 0
+    Datesindata = datesindata.to_frame(index= False, name = "Date")
+    
+    for i in range(NumDays):
+            hi = Datatoplot.at[i, "Date"]
+            for j in range(len(Datesindata)):
+                bye = Datesindata.at[j, "Date"]
+                
+                if hi == bye:
+                    Datatoplot.at[i, "Values"] = valuesindata[j]
+                    
+    Matplotlibdates = matplotlib.dates.date2num(Datatoplot.Date)
+    Valuestoplot = Datatoplot.Values
+    return Matplotlibdates, Valuestoplot
---
+++ Charts/helper.py
@@ -0,0 +1,81 @@
+from app import App
+import pyodbc
+import pandas as pd
+import datetime
+
+def connect_to_fetch_all_ata(from_dt, to_dt):
+    conn = pyodbc.connect(driver=App().db_driver, host=App().hostname, database=App().db_name,
+                              user=App().db_username, password=App().db_password)
+    all_ata_query = "SELECT DISTINCT ATA_Main from Airline_MDC_Data WHERE DateAndTime BETWEEN '" + from_dt + "' AND '" + to_dt + "'"
+    try:
+        all_ata_df = pd.read_sql(all_ata_query, conn)
+
+        return all_ata_df
+    except pyodbc.Error as err:
+        print("Couldn't connect to Server")
+        print("Error message:- " + str(err))
+
+
+def fetch_all_eqids(from_dt, to_dt):
+    conn = pyodbc.connect(driver=App().db_driver, host=App().hostname, database=App().db_name,
+                              user=App().db_username, password=App().db_password)
+    all_ata_query = "SELECT DISTINCT Equation_ID from Airline_MDC_Data WHERE DateAndTime BETWEEN '" + from_dt + "' AND '" + to_dt + "'"
+    try:
+        all_eqid_df = pd.read_sql(all_ata_query, conn)
+
+        return all_eqid_df
+    except pyodbc.Error as err:
+        print("Couldn't connect to Server")
+        print("Error message:- " + str(err))
+
+# def connect_database_for_chart3(aircraft_no, equation_id, CurrentFlightPhaseEnabled, from_dt, to_dt):
+
+
+#     if CurrentFlightPhaseEnabled == 0: # Flight phase is NOT enabled
+#         sql = "SELECT COUNT(*) AS OccurencesPerDay, cast(DateAndTime as DATE) AS Dates from Airline_MDC_Data WHERE Equation_ID='"+equation_id+"' AND aircraftno = '"+str(aircraft_no)+"' AND Flight_Phase IS NOT NULL AND DateAndTime BETWEEN '"+from_dt+"' AND '"+to_dt+"' GROUP BY cast(DateAndTime as DATE)"
+#     elif CurrentFlightPhaseEnabled == 1:
+#         sql = "SELECT COUNT(*) AS OccurencesPerDay, cast(DateAndTime as DATE) AS Dates from Airline_MDC_Data WHERE Equation_ID='"+equation_id+"' AND aircraftno = '"+str(aircraft_no)+"' AND Flight_Phase IS NULL AND DateAndTime BETWEEN '"+from_dt+"' AND '"+to_dt+"' GROUP BY cast(DateAndTime as DATE)"
+#     print(sql)
+#     try:
+#         conn = pyodbc.connect(driver=App().db_driver, host=App().hostname, database=App().db_name,
+#                               user=App().db_username, password=App().db_password)
+#         chart3_sql_df = pd.read_sql(sql, conn)
+#         # MDCdataDF.columns = column_names
+#         conn.close()
+#         return chart3_sql_df
+#     except pyodbc.Error as err:
+#         print("Couldn't connect to Server")
+#         print("Error message:- " + str(err))
+
+
+def connect_database_for_chart3(aircraft_no, equation_id, CurrentFlightPhaseEnabled, from_dt, to_dt):
+    conn = pyodbc.connect(driver=App().db_driver, host=App().hostname, database=App().db_name,
+                              user=App().db_username, password=App().db_password)
+    if equation_id == 'NONE':
+        all_eqid = fetch_all_eqids(from_dt, to_dt)
+
+        all_eqid_str = "("
+        all_eqid_list = all_eqid['Equation_ID'].tolist()
+        for each_eqid in all_eqid_list:
+            #all_eqid_str_list.append(str(each_eqid))
+            all_eqid_str += "'" + str(each_eqid) + "'"
+            if each_eqid != all_eqid_list[-1]:
+                all_eqid_str += ","
+            else:
+                all_eqid_str += ")"
+
+        equation_id = all_eqid_str
+    
+    if CurrentFlightPhaseEnabled == 0:
+        sql = "SELECT * FROM Airline_MDC_Data WHERE Equation_ID NOT IN " + equation_id + " AND aircraftno = " + str(aircraft_no) + " AND DateAndTime BETWEEN '" + from_dt + "' AND '" + to_dt + "'"
+    else:
+        sql = "SELECT * FROM Airline_MDC_Data WHERE Equation_ID IN " + equation_id + " AND aircraftno = " + str(aircraft_no) + " AND DateAndTime BETWEEN '" + from_dt + "' AND '" + to_dt + "'"
+
+    print('Chart3 SQL ', sql)
+    try:
+        chart3_sql_df = pd.read_sql(sql, conn)
+        # conn.close()
+        return chart3_sql_df
+    except pyodbc.Error as err:
+        print("Couldn't connect to Server")
+        print("Error message:- " + str(err))
