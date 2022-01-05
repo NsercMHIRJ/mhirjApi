fd6d14854dd152c68c22aca16669550b5d59a40d
NavneetBhatti <navneetbhatti13@gmail.com>
2022-01-04 18:15:41 -0600

    jam report

--- GenerateReport/jamReport.py
+++ GenerateReport/jamReport.py
@@ -10,17 +10,23 @@ def mdcDF(MaxAllowedOccurrences: int, MaxAllowedConsecLegs: int, MaxAllowedInter
     MDCdataDF.sort_values(by= "DateAndTime", ascending= False, inplace= True, ignore_index= True)
     return MDCdataDF
 
+# this list can be updated to include other messages
+listofJamMessages = ["B1-309178","B1-309179","B1-309180","B1-060044","B1-060045","B1-007973",
+                     "B1-060017","B1-006551","B1-240885","B1-006552","B1-006553","B1-006554",
+                     "B1-006555","B1-007798","B1-007772","B1-240938","B1-007925","B1-007905",
+                     "B1-007927","B1-007915","B1-007926","B1-007910","B1-007928","B1-007920"]    
+
 
  
-def jamReport(OutputTableHistory, ACSN_chosen,MDCdataDF):
+def jamReport(OutputTableHistory, ACSN_chosen,MDCdataDF,listofmessages= listofJamMessages):
 
-   listofJamMessages = list()
-   all_jam_messages = connect_to_fetch_all_jam_messages()
-   for each_jam_message in all_jam_messages['Jam_Message']:
-       listofJamMessages.append(each_jam_message)
+#    listofJamMessages = list()
+#    all_jam_messages = connect_to_fetch_all_jam_messages()
+#    for each_jam_message in all_jam_messages['Jam_Message']:
+#        listofJamMessages.append(each_jam_message)
  
    datatofilter = MDCdataDF.copy(deep=True)
-   isin = OutputTableHistory["B1-Equation"].isin(listofJamMessages)
+   isin = OutputTableHistory["B1-Equation"].isin(listofmessages)
  
    filter1 = OutputTableHistory[isin][["AC SN", "B1-Equation"]]
    listoftuplesACID = list(zip(filter1["AC SN"], filter1["B1-Equation"]))
